11b6baeb5cde66137b92f8645d2520525069c935
Psyryll <Psyryll@psyryllj@gmail.com>
2023-03-17 23:27:21 +0800

    Changes made

--- cypress/e2e/variables/settings.js
+++ cypress/e2e/variables/settings.js
@@ -1,7 +1,8 @@
 export const username = 'User' + Math.floor((Math.random() * 1000) + 1) + '@gmail.com'
-export const web = 'http://127.0.0.1:8000/';
+export const web = 'https://hammerhead-app-wwgsk.ondigitalocean.app/';
 export const userData = {
     username: 'demo',
     password: "demo",
     ValidateData: "Validate Data",
   }
+ 
